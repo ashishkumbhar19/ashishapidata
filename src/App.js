856c927d7cc281d12f3cbeeabcd8f5fc2a72e35b
Diwakar Chavan <kadampratiksha761@gmail.com>
2024-02-23 16:14:01 +0530

    Added my project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+
 
 function App() {
+  const [productData, setProductData] = useState(null);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://dummyjson.com/products/1');
+      setProductData(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Product Details</h1>
+      {productData ? (
+        <div>
+          <h2>{productData.title}</h2>
+          <p><strong>Description:</strong> {productData.description}</p>
+          <p><strong>Price:</strong> ${productData.price}</p>
+          <p><strong>Discount Percentage:</strong> {productData.discountPercentage}%</p>
+          <p><strong>Rating:</strong> {productData.rating}</p>
+          <p><strong>Stock:</strong> {productData.stock}</p>
+          <p><strong>Brand:</strong> {productData.brand}</p>
+          <p><strong>Category:</strong> {productData.category}</p>
+          <img src={productData.thumbnail} alt="Thumbnail" />
+          <h3>Images</h3>
+          <ul>
+            {productData.images.map((image, index) => (
+              <li key={index}>
+                <img src={image} alt={`Product ${index + 1}`} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
